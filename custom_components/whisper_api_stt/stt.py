d64c89296bbe509f3952708d18375d602fe7dfe6
Zhaky Hadi <zhaky.hadi@gmail.com>
2024-07-28 11:01:18 +0700

    change OpenAI server pointing

--- custom_components/whisper_api_stt/stt.py
+++ custom_components/whisper_api_stt/stt.py
@@ -26,7 +26,7 @@ import io
 
 CONF_API_KEY = 'api_key'
 DEFAULT_LANG = 'en-US'
-OPENAI_STT_URL = "https://api.openai.com/v1/audio/transcriptions"
+OPENAI_STT_URL = "https://ai.dev0.my.id/v1/audio/transcriptions"
 CONF_MODEL = 'model'
 CONF_URL = 'url'
 CONF_PROMPT = 'prompt'
